f22e80aa46dbc550d220c60127655470d4367b07
Jirayu <samurai_ice007@hotmail.com>
2026-03-17 17:23:59 +0700

    add font and devtool

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,34 @@ import './index.css'
 import App from './App.tsx'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 const queryClient = new QueryClient();
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Noto Sans Thai',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
