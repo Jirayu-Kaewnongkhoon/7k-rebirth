75b549a65ed155e483d904f3e5f2a38514b2145d
Jirayu-Kaewnongkhoon <47939767+Jirayu-Kaewnongkhoon@users.noreply.github.com>
2026-04-01 15:30:38 +0700

    add pool to prisma

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,15 +1,18 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient, STATE } from "./generated/prisma/client";
+import pg from "pg";
 
 const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
 
-const adapter = new PrismaPg({
+const pool = new pg.Pool({
     connectionString,
     ssl: {
         rejectUnauthorized: false
     }
 });
+
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma, STATE };
