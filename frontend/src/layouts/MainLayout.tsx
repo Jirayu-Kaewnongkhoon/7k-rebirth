85bf0ef71b461469fa4070485625ee4d048b8e52
Jirayu <samurai_ice007@hotmail.com>
2026-06-28 22:43:28 +0700

    fix scrollbar

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,7 +1,26 @@
+import { useLocation } from "react-router";
+import { useEffect } from "react";
+
 import Sidebar from "../components/Sidebar/Sidebar";
 
 function MainLayout() {
-    return <Sidebar />
+    return (
+        <>
+            <Sidebar />
+            <ScrollToTop />
+        </>
+    )
 }
 
 export default MainLayout
+
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
